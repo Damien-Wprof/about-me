f4188af24401051797afc98de3ea68a3839f6125
damien welch <nomad2142@protonmail.com>
2026-02-03 14:41:05 -0800

    added new js, json and html files

---
+++ app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+
+
+//let usrName = prompt("What's you're name?")
+prompt("Do you like cheese?")
+prompt("Do you like sunshine")
+prompt("Do you like pickles?")
+prompt("Do you like cats?")
+prompt("Do you like dogs?")
