73d23fe5efb0baeb2b2a4abf2b910a6ecaff5f3a
damien welch <nomad2142@protonmail.com>
2026-02-03 15:29:01 -0800

    added new js code

--- app.js
+++ app.js
@@ -1,10 +1,56 @@
 'use strict';
 
 
+let usrName = prompt("What's you're name?")
 
-//let usrName = prompt("What's you're name?")
-prompt("Do you like cheese?")
-prompt("Do you like sunshine")
-prompt("Do you like pickles?")
-prompt("Do you like cats?")
-prompt("Do you like dogs?")
+let vidGame = prompt(usrName + ", Do I like video games? (yes/no)").toLowerCase();
+
+if (vidGame === "yes" || vidGame === "y") {
+    alert("Correct!");
+} else if (vidGame === "no" || vidGame === "n") {
+    alert ("Wrong!")
+} else {
+    alert ("Please answer with 'Yes' or 'No'")
+} console.log("I love video games for you're information!")
+
+let cat = prompt(usrName + ", Am I a cat person? (yes/no)").toLowerCase();
+
+if (cat === "yes" || cat === "y") {
+    alert("Correct!");
+} else if (cat === "no" || cat === "n") {
+    alert ("Wrong!")
+} else {
+    alert ("Please answer with 'Yes' or 'No'")
+} console.log("I love my cat jack, he's a cutie pie")
+
+let food = prompt(usrName + ", Do I like food? (yes/no)").toLowerCase();
+
+if (food === "yes" || food === "y") {
+    alert("Correct!");
+} else if (food === "no" || food === "n") {
+    alert ("Wrong!")
+} else {
+    alert ("Shouldn't it be obvious?")
+} console.log ("This *should* be obvious")
+
+let mom = prompt(usrName +", Isn't my favorite person my mom? (yes/no)").toLowerCase();
+
+if (mom === "yes" || mom === "y") {
+    alert("Correct!");
+} else if (mom === "no" || mom === "n") {
+    alert ("Wrong!")
+} else {
+    alert ("Please answer with 'Yes' or 'No'")
+} console.log("My mom is my favorite")
+
+let sleep = prompt(usrName + ", Don't I love to sleep? (yes/no)").toLowerCase();
+
+if (sleep === "yes" || sleep === "y") {
+    alert("Correct!");
+} else if (sleep === "no" || sleep === "n") {
+    alert ("Wrong!")
+} else {
+    alert ("Please answer with 'Yes' or 'No'")
+} console.log('I can\'t wait to sleep, even now!')
+
+alert ('Welcome ' + usrName)
